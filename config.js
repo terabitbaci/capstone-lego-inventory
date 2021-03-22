d2bc5d4aba1c3dbb8aa36d2001917c1d357985cb
terabitbaci <teresabacigalupi@gmail.com>
2021-03-21 18:23:55 -0700

    change Atlas connection to a writable database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://admin:administrator1@ds163530.mlab.com:63530/lego-inventory';
 //exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:administrator1@ds163530.mlab.com:63530/lego-inventory';
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://lego-inventory-user:Password1@lego-inventory-cluster.uvoen.mongodb.net/admin?retryWrites=true&w=majority';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://lego-inventory-user:Password1@lego-inventory-cluster.uvoen.mongodb.net/admin?retryWrites=true&w=majority';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://lego-inventory-user:Password1@lego-inventory-cluster.uvoen.mongodb.net/lego-inventory-for-heroku?retryWrites=true&w=majority';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://lego-inventory-user:Password1@lego-inventory-cluster.uvoen.mongodb.net/lego-inventory-for-heroku?retryWrites=true&w=majority';
 exports.PORT = process.env.PORT || 8080;
